61afa1fda3344ea3f4e26568d89ffb0c95f2486d
Shubham Verma <35617607+sverma1999@users.noreply.github.com>
2018-01-22 11:58:22 -0400

    FirstUplaod

---
+++ app/src/main/java/com/practice/shubham/unbinternationalstudents/unbisa.java
@@ -0,0 +1,23 @@
+package com.practice.shubham.unbinternationalstudents;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+/**
+ * Created by Shubham on 11/26/2017.
+ */
+
+public class unbisa extends Fragment {
+
+    View myView;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedIn) {
+    myView = inflater.inflate(R.layout.unbisa, container, false);
+    return myView;
+    }
+
+
+}
---
+++ app/src/main/java/com/practice/shubham/unbinternationalstudents/welcomeScreen.java
@@ -0,0 +1,42 @@
+package com.practice.shubham.unbinternationalstudents;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class welcomeScreen extends AppCompatActivity {
+    private TextView tv;
+    private ImageView iv;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+        tv = (TextView) findViewById(R.id.welcomeText);
+        iv = (ImageView) findViewById(R.id.unbISALogo);
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.mytransition);
+        tv.startAnimation(myanim);
+        iv.startAnimation(myanim);
+        final Intent i = new Intent(this,FirstScreen.class);
+        Thread timer = new Thread(){
+            public void run(){
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+
+    }
+}
